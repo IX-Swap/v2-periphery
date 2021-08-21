01e940502264919d3e758a522ded898e844ee67a
AlexanderC <alexander.moldova@gmail.com>
2021-08-21 12:15:40 +0300

    Adding missing liq methods

--- contracts/interfaces/IIxsV2LiquidityRouter.sol
+++ contracts/interfaces/IIxsV2LiquidityRouter.sol
@@ -79,4 +79,23 @@ interface IIxsV2LiquidityRouter {
         bytes32 r,
         bytes32 s
     ) external returns (uint256 amountToken, uint256 amountETH);
+
+    function removeLiquidityETHSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountETH);
+    
+    function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external returns (uint amountETH);
 }
